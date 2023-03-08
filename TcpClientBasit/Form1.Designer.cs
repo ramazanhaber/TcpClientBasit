535615d0ff1a115fc6d68573757ff3f3c140e9c3
Ramazan HABER <ramazanhaberr@gmail.com>
2023-03-08 11:30:57 +0300

    Add project files.

---
+++ TcpClientBasit/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TcpClientBasit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMesaj = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDonenMesaj = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMesaj
+            // 
+            this.txtMesaj.Location = new System.Drawing.Point(139, 12);
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(221, 20);
+            this.txtMesaj.TabIndex = 0;
+            this.txtMesaj.Text = "MERHABALAR";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gönderilecek Mesaj : ";
+            // 
+            // txtDonenMesaj
+            // 
+            this.txtDonenMesaj.Location = new System.Drawing.Point(139, 47);
+            this.txtDonenMesaj.Multiline = true;
+            this.txtDonenMesaj.Name = "txtDonenMesaj";
+            this.txtDonenMesaj.Size = new System.Drawing.Size(242, 124);
+            this.txtDonenMesaj.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dönen Mesaj : ";
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(139, 186);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(242, 23);
+            this.btnGonder.TabIndex = 2;
+            this.btnGonder.Text = "GÖNDER";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 253);
+            this.Controls.Add(this.btnGonder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDonenMesaj);
+            this.Controls.Add(this.txtMesaj);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tcp Client Basit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMesaj;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDonenMesaj;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGonder;
+    }
+}
+
---
+++ TcpClientBasit/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcpClientBasit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TcpClient tcpClient;
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tcpClient = new TcpClient("127.0.0.1", 2015);
+            }
+            catch
+            {
+                Console.WriteLine("Baglanamadi");
+                return;
+            }
+
+            var nStream = tcpClient.GetStream();
+            var reader = new StreamReader(nStream);
+            var writer = new StreamWriter(nStream);
+            string readedStr;
+            writer.WriteLine(txtMesaj.Text);
+            writer.Flush();
+            readedStr = reader.ReadLine();
+            txtDonenMesaj.Text = readedStr;
+
+            try
+            {
+                writer.Close();
+                reader.Close();
+                nStream.Close();
+            }
+
+            catch
+            {
+                Console.WriteLine("Düzgün kapatilamiyor");
+            }
+        }
+    }
+}
